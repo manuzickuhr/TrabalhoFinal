c76f9007b0370eeca41b123e70f91414b58a4328
manu <141519903+manuzickuhr@users.noreply.github.com>
2025-01-14 13:31:46 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -38,6 +38,20 @@ class Usuario:
     def senha(self, novo_senha):
         self.__senha = novo_senha
 
+    def listar_usuarios():
+        try:
+            cursor = conexao.cursor()
+            query = "SELECT * FROM Usuario"
+            cursor.execute(query)
+            resultados = cursor.fetchall()
+            
+            for row in resultados:
+                print(row)
+        except mysql.connector.Error as e:
+            print(f"Erro ao listar usuários: {e}")
+        finally:
+            cursor.close()
+
 class Funcionario(Usuario):
     def __init__(self, id, usuario, senha, cpf, primeiroNome, sobrenome, email, funcao):
         super().__init__(id, usuario, senha,cpf, primeiroNome, sobrenome)
@@ -108,14 +122,128 @@ class Funcionario(Usuario):
         finally: 
             cursor.close()
 
-    def editar_funcionario(): #somente com a funcao gerente
-        print('editar funcionario')
-    
-    def editar_cliente():
-        print('editar cliente')
+    def editar_funcionario(id, funcionario, usuario=None, senha=None, cpf=None, primeiroNome=None, sobrenome=None, email=None, funcao=None):
+        if funcionario.funcao == "Gerente":
+            try: 
+                cursor = conexao.cursor()
 
+                campos_atualizar = []
+                dados = []
+
+                campos_funcionario = []
+                dados_funcionario = []
+
+                if usuario:
+                    campos_atualizar.append("usuario = %s")
+                    dados.append(usuario)
+                if senha:
+                    campos_atualizar.append("senha = %s")
+                    dados.append(senha)
+                if cpf:
+                    campos_atualizar.append("cpf = %s")
+                    dados.append(cpf)
+                if primeiroNome:
+                    campos_atualizar.append("primeiroNome = %s")
+                    dados.append(primeiroNome)
+                if sobrenome:
+                    campos_atualizar.append("sobrenome = %s")
+                    dados.append(sobrenome)
+                if email:
+                    campos_funcionario.append("email = %s")
+                    dados_funcionario.append(email)
+                if funcao:
+                    campos_funcionario.append("funcao = %s")
+                    dados_funcionario.append(funcao)
+                
+                if not campos_atualizar and not campos_funcionario:
+                    print("Nenhuma informação fornecida para atualização.")
+                    return
+                
+                # Atualizando dados do usuário
+                if campos_atualizar:
+                    query_usuario = f"""UPDATE usuario
+                        SET {', '.join(campos_atualizar)}
+                        WHERE id = %s""" 
+                    dados.append(id)
+                    cursor.execute(query_usuario, dados)
+                    conexao.commit()
+
+                # Atualizando dados do funcionário
+                if campos_funcionario:
+                    query_funcionario = f"""UPDATE funcionario
+                        SET {', '.join(campos_funcionario)}
+                        WHERE id = %s"""
+                    dados_funcionario.append(id)
+                    cursor.execute(query_funcionario, dados_funcionario)
+                    conexao.commit()
+
+                print("Informações do funcionário atualizadas com sucesso!")
+
+            except mysql.connector.Error as erro:
+                print(f"Erro ao atualizar o funcionário: {erro}")
+
+            finally:
+                cursor.close()
+                
+    def editar_cliente(id, usuario=None, senha=None, cpf=None, primeiroNome=None, sobrenome=None, emprestimos=None):
+        try: 
+            cursor = conexao.cursor()
+
+            campos_atualizar = []
+            dados = []
+
+            campos_cliente = []
+            dados_cliente = []
+
+            if usuario:
+                    campos_atualizar.append("usuario = %s")
+                    dados.append(usuario)
+            if senha:
+                    campos_atualizar.append("senha = %s")
+                    dados.append(senha)
+            if cpf:
+                    campos_atualizar.append("cpf = %s")
+                    dados.append(cpf)
+            if primeiroNome:
+                    campos_atualizar.append("primeiroNome = %s")
+                    dados.append(primeiroNome)
+            if sobrenome:
+                    campos_atualizar.append("sobrenome = %s")
+                    dados.append(sobrenome)
+            if emprestimos:
+                campos_cliente.append('emprestimos = %s')
+                dados_cliente.append(emprestimos)
+
+            if not campos_atualizar and not campos_cliente:
+                    print("Nenhuma informação fornecida para atualização.")
+                    return
+
+            if campos_atualizar:
+                    query_usuario = f"""UPDATE usuario
+                        SET {', '.join(campos_atualizar)}
+                        WHERE id = %s""" 
+                    dados.append(id)
+                    cursor.execute(query_usuario, dados)
+                    conexao.commit()
+
+            if campos_cliente:
+                query_cliente = f"""UPDATE cliente
+                    SET {','.join(campos_cliente)}
+                    WHERE id = %s"""
+                dados_cliente.append(id)
+                cursor.execute(query_cliente, dados_cliente)
+                conexao.commit()
+
+            print("Informações do cliente atualizadas com sucesso!")
+
+        except mysql.connector.Error as erro:
+                print(f"Erro ao atualizar o cliente: {erro}")
+
+        finally:
+                cursor.close()
+            
     def excluir_funcionario(valor, funcionario): #somente gerente
-        if funcionario.funcao == "gerente":
+        if funcionario.funcao == "Gerente":
             try:
                 cursor = conexao.cursor()
                 query_funcionario = f"DELETE FROM funcionario WHERE id =%s"
@@ -155,26 +283,65 @@ class Funcionario(Usuario):
         finally:
             cursor.close()
 
+    def listar_funcionarios():
+        try:
+            cursor = conexao.cursor()
+            query = "SELECT * FROM usuario u, funcionario f WHERE u.id = f.id and u.tipo = 'Funcionario'"
+            cursor.execute(query)
+            resultados = cursor.fetchall()
+            
+            for row in resultados:
+                print(row)
+        except mysql.connector.Error as e:
+            print(f"Erro ao listar funcionarios: {e}")
+        finally:
+            cursor.close()
+
+    def listar_clientes():
+        try:
+            cursor = conexao.cursor()
+            query = "SELECT * FROM usuario u, cliente c WHERE u.id = c.id and u.tipo = 'Cliente'"
+            cursor.execute(query)
+            resultados = cursor.fetchall()
+            
+            for row in resultados:
+                print(row)
+        except mysql.connector.Error as e:
+            print(f"Erro ao listar funcionarios: {e}")
+        finally:
+            cursor.close()
+
 class Cliente(Usuario):
     def __init__(self, id, usuario, senha, cpf, primeiroNome, sobrenome, emprestimos):
         super().__init__(id, usuario, senha, cpf, primeiroNome, sobrenome)
         self.emprestimos = emprestimos
 
     def ver_livros():
-        print('Ver a lista de todos os livros')
+        try:
+            cursor = conexao.cursor()
+            query = "SELECT * FROM livro"
+            cursor.execute(query)
+            resultados = cursor.fetchall()
+            
+            for row in resultados:
+                print(row)
+        except mysql.connector.Error as e:
+            print(f"Erro ao listar livros: {e}")
+        finally:
+            cursor.close()
     
     def ver_emprestimos():
         print('Ver os emprestimos linkados aquele cliente')
 
 class Livro():
-    def __init__(self, id, isbn, nome, autor, edicao, quantidade):
+    def __init__(self, id, isbn, nome, autor, edicao, quantidade, emprestado):
         self.id = id
         self.isbn = isbn 
         self.nome = nome
         self.autor = autor
         self.edicao = edicao
         self.quantidade = quantidade
-        self.emprestado = False
+        self.emprestado = emprestado
 
     def adicionar_livro(conexao, isbn, nome, autor, edicao, quantidade):
         try:
@@ -195,8 +362,50 @@ class Livro():
         finally:
             cursor.close()
 
-    def editar_livro():
-        print('Editar livro')
+    def editar_livro(id, isbn = None, nome=None, autor=None, edicao=None, quantidade=None, emprestado=None):
+        try:
+            cursor = conexao.cursor()
+            campos_atualizar = []
+            dados= []
+
+            if isbn:
+                campos_atualizar.append('isbn = %s')
+                dados.append(isbn)
+            if nome:
+                campos_atualizar.append('nome = %s')
+                dados.append(nome)
+            if autor:
+                campos_atualizar.append('autor = %s')
+                dados.append(autor)
+            if edicao:
+                campos_atualizar.append('edicao = %s ')
+                dados.append(edicao)
+            if quantidade:
+                campos_atualizar.append('quantidade = %s')
+                dados.append(quantidade)
+            if emprestado:
+                campos_atualizar.append('emprestado = %s')
+                dados.append(emprestado)
+
+            if not campos_atualizar:
+                    print("Nenhuma informação fornecida para atualização.")
+                    return
+
+            if campos_atualizar:
+                    query = f"""UPDATE livro
+                        SET {', '.join(campos_atualizar)}
+                        WHERE id = %s""" 
+                    dados.append(id)
+                    cursor.execute(query, dados)
+                    conexao.commit()
+            
+            print("Informações do livro atualizadas com sucesso!")
+
+        except mysql.connector.Error as erro:
+                print(f"Erro ao atualizar o livro: {erro}")
+
+        finally:
+                cursor.close()
 
     def excluir_livro(conexao, valor):
         try:
@@ -236,6 +445,8 @@ class Emprestimo():
         print('limite')
 
 
+
+#Testes de implementação
 cliente1 = Cliente(
     id=1,
     usuario="joao123",
@@ -246,7 +457,7 @@ cliente1 = Cliente(
     emprestimos=0
 )
 
-Funcionario.cadastrar_cliente(
+'''Funcionario.cadastrar_cliente(
     conexao=conexao,
     usuario=cliente1.usuario,
     senha=cliente1.senha,
@@ -255,29 +466,6 @@ Funcionario.cadastrar_cliente(
     sobrenome=cliente1.sobrenome,
     tipo="cliente",
     emprestimos=cliente1.emprestimos
-)
-
-funcionario1 = Funcionario(
-    id=1,
-    usuario="ana456",
-    senha="senha456",
-    cpf="98765432100",
-    primeiroNome="Ana",
-    sobrenome="Pereira",
-    email="ana.pereira@email.com",
-    funcao="Gerente"
-)
-
-'''Funcionario.cadastrar_funcionario(
-    conexao=conexao,
-    usuario=funcionario1.usuario,
-    senha=funcionario1.senha,
-    cpf=funcionario1.cpf,
-    primeiro_nome=funcionario1.primeiroNome,
-    sobrenome=funcionario1.sobrenome,
-    tipo="funcionario",
-    email=funcionario1.email,
-    funcao=funcionario1.funcao
 )'''
 
 funcionario1 = Funcionario(
@@ -290,6 +478,9 @@ funcionario1 = Funcionario(
     email="ana.pereira@email.com",
     funcao="Gerente"
 )
+
+Funcionario.editar_funcionario(id=3, funcionario=funcionario1, email="anapereira@hotmail.com")
+Funcionario.editar_cliente(id=6, emprestimos=2)
 
 '''Funcionario.cadastrar_funcionario(
     conexao=conexao,
@@ -305,6 +496,13 @@ funcionario1 = Funcionario(
 
 #Funcionario.excluir_cliente(1)
 
-Livro.adicionar_livro(conexao, "123", "Exemplo de Livro", "Autor X", "1ª Edição", 10)
+#Livro.adicionar_livro(conexao, "123", "Exemplo de Livro", "Autor X", "1ª Edição", 10)
+
+#Livro.editar_livro(1, emprestado=2 )
+
+#Usuario.listar_usuarios()
+#Cliente.ver_livros()
+
+Funcionario.listar_clientes()
 
 conexao.close()
